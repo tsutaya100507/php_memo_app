242949890b739aae5b23b2e0549820d585edace5
Tatsuya Ichikawa <>
2019-04-09 08:42:31 +0900

    [Add] Add pagination

--- index.php
+++ index.php
@@ -20,7 +20,16 @@
   <?php
     require('dbconnect.php');
 
-    $memos = $db->query('SELECT * FROM memos ORDER BY id DESC LIMIT 0, 5');
+    if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
+      $page  = $_REQUEST['page'];
+    } else {
+      $page = 1;
+    }
+    $start = 5 * ($page - 1);
+
+    $memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC LIMIT ?, 5');
+    $memos->bindParam(1, $start, PDO::PARAM_INT);
+    $memos->execute();
   ?>
   <article>
     <?php while ($memo = $memos->fetch()): ?>
@@ -29,7 +38,20 @@
       <hr>
     <?php endwhile; ?>
   </article>
+
   <a href="input.html">メモ入力</a>
+  <?php if ($page >= 2): ?>
+  <a href="index.php?page=<?php print($page-1); ?>"><?php print($page-1); ?>ページ目へ</a>
+  <?php endif; ?>
+   |
+  <?php
+  $counts = $db->query('SELECT COUNT(*) as cnt FROM memos');
+  $count  = $counts->fetch();
+  $max_page = ceil($count['cnt'] / 5);
+  if ($page < $max_page):
+  ?>
+  <a href="index.php?page=<?php print($page+1); ?>"><?php print($page+1); ?>ページ目へ</a>
+  <?php endif; ?>
 </main>
 </body>    
 </html>
